6a036c9f782328816bebe4dce800d1e2473bb14e
10gic <2391796+10gic@users.noreply.github.com>
2024-05-04 17:25:41 +0800

    Update 3rd party deps

---
+++ third_party/windows/OpenCL-SDK-v2023.12.14-Win-x64/share/cmake/OpenCLICDLoader/OpenCLICDLoaderTargets-debug.cmake
@@ -0,0 +1,28 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Debug".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "OpenCL::OpenCL" for configuration "Debug"
+set_property(TARGET OpenCL::OpenCL APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(OpenCL::OpenCL PROPERTIES
+  IMPORTED_IMPLIB_DEBUG "${_IMPORT_PREFIX}/lib/OpenCL.lib"
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/OpenCL.dll"
+  )
+
+list(APPEND _cmake_import_check_targets OpenCL::OpenCL )
+list(APPEND _cmake_import_check_files_for_OpenCL::OpenCL "${_IMPORT_PREFIX}/lib/OpenCL.lib" "${_IMPORT_PREFIX}/bin/OpenCL.dll" )
+
+# Import target "OpenCL::cllayerinfo" for configuration "Debug"
+set_property(TARGET OpenCL::cllayerinfo APPEND PROPERTY IMPORTED_CONFIGURATIONS DEBUG)
+set_target_properties(OpenCL::cllayerinfo PROPERTIES
+  IMPORTED_LOCATION_DEBUG "${_IMPORT_PREFIX}/bin/cllayerinfo.exe"
+  )
+
+list(APPEND _cmake_import_check_targets OpenCL::cllayerinfo )
+list(APPEND _cmake_import_check_files_for_OpenCL::cllayerinfo "${_IMPORT_PREFIX}/bin/cllayerinfo.exe" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
